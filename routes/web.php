0b1592ea3a265719f8d78d5b062d9da7e8a0d38e
Shahriar Ahmed <shahriarabiddut@gmail.com>
2025-03-08 04:07:11 +0600

    Assign User Role To Every User

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Enum\PermissionsEnum;
+use App\Enum\RolesEnum;
 use App\Http\Controllers\CommentController;
 use App\Http\Controllers\FeatureController;
 use App\Http\Controllers\ProfileController;
@@ -8,7 +9,7 @@ use App\Http\Controllers\UpvoteController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::redirect('/','/dashboard');
+Route::redirect('/', '/dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -16,20 +17,22 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // Another Middleware
-    Route::middleware(['verified'])->group(function () {
-        Route::get('/dashboard', function () { return Inertia::render('Dashboard');})->name('dashboard');
+    Route::middleware(['verified', 'role:' . RolesEnum::User->value])->group(function () {
+        Route::get('/dashboard', function () {
+            return Inertia::render('Dashboard');
+        })->name('dashboard');
         Route::resource('feature', FeatureController::class)
-        ->except(['index','show'])
-        ->middleware('can:'.PermissionsEnum::ManageFeatures->value);
+            ->except(['index', 'show'])
+            ->middleware('can:' . PermissionsEnum::ManageFeatures->value);
         Route::get('/feature', [FeatureController::class, 'index'])->name('feature.index');
         Route::get('/feature/{feature}', [FeatureController::class, 'show'])->name('feature.show');
         // Vote
         Route::post('/feature/{feature}/upvote', [UpvoteController::class, 'store'])->name('feature.vote');
         Route::delete('/upvote/{feature}', [UpvoteController::class, 'destroy'])->name('upvote.destroy');
         // Comments
-        Route::post('/feature/{feature}/comments', [CommentController::class, 'store'])->name('comments.store')->middleware('can:'.PermissionsEnum::ManageComments->value);
-        Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comment.destroy')->middleware('can:'.PermissionsEnum::ManageComments->value);;
+        Route::post('/feature/{feature}/comments', [CommentController::class, 'store'])->name('comments.store')->middleware('can:' . PermissionsEnum::ManageComments->value);
+        Route::delete('/comments/{comment}', [CommentController::class, 'destroy'])->name('comment.destroy')->middleware('can:' . PermissionsEnum::ManageComments->value);;
     });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
